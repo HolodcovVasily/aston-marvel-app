9cff78bfee236010ab26f9ce990a219d5739896d
HolodcovVasily <vasil.holodcov@gmail.com>
2023-07-14 10:44:53 +0300

    add redux toolkit and redux-reac libs, add wrap my App.tsx in Provider and create store in dir  store/index.ts. Add Tailwind lib as css-framework

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-screen w-full md:w-auto bg-slate-900">
+      <h1 className="text-center pt-5 text-gray-200 font-bold">Marvel App</h1>
     </div>
   );
 }
