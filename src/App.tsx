7a7fb426328ec45c100d6d58842fdbdb0b34640e
HolodcovVasily <vasil.holodcov@gmail.com>
2023-07-14 15:40:15 +0300

    add some eslint rules to .eslintrc file

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import { HomePage } from "./pages/HomePage/HomePage";
-import { Favorites } from "./pages/Favorites/Favorites";
-import { SignIn } from "./pages/Sign In/SignIn";
-import SignUp from "./pages/Sign Up/SignUp";
-import { Header } from "./components/Header/Header";
-import { Footer } from "./pages/Footer/Footer";
+import { HomePage } from './pages/HomePage/HomePage';
+import { Favorites } from './pages/Favorites/Favorites';
+import { SignIn } from './pages/Sign In/SignIn';
+import SignUp from './pages/Sign Up/SignUp';
+import { Header } from './components/Header/Header';
+import { Footer } from './pages/Footer/Footer';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <Header />
       <Routes>
-        <Route path="/signin" element={<SignIn />}></Route>
-        <Route path="/signup" element={<SignUp />}></Route>
-        <Route path="/" element={<HomePage />}></Route>
-        <Route path="/favorites" element={<Favorites />}></Route>
+        <Route path='/signin' element={<SignIn />}></Route>
+        <Route path='/signup' element={<SignUp />}></Route>
+        <Route path='/' element={<HomePage />}></Route>
+        <Route path='/favorites' element={<Favorites />}></Route>
       </Routes>
       <Footer />
     </>
