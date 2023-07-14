47db913aca51a9f0c1a75653a8c2026da04d3741
HolodcovVasily <vasil.holodcov@gmail.com>
2023-07-14 13:11:50 +0300

    add prettier to project

--- src/pages/Sign In/SignIn.tsx
+++ src/pages/Sign In/SignIn.tsx
@@ -1,5 +1,5 @@
-import React from "react";
+import React from 'react';
 
 export function SignIn() {
-  return <div className="flex-auto">Sign In</div>;
+  return <div className='flex-auto'>Sign In</div>;
 }
